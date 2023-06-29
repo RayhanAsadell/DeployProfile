85efdadf35468f23e3b17650be27e89d93cfaadf
Rayhan Asadel <rayhan.asadel@tiket.com>
2023-06-29 17:28:44 +0700

    change profile source and port

--- app.js
+++ app.js
@@ -6,9 +6,9 @@ const app = express();
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
+    res.sendFile("./public/index.html");
 });
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+app.listen(8000, () => {
+    console.log('Server is running on port 8000');
 });
